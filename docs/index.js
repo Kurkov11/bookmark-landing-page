da9259f2a4999c1a736d8a3f6fb6bf3b148128f8
Kurkov11 <kurkovsw@icloud.com>
2024-01-23 10:36:06 +0100

    Add code that shows and hides correct arrow when question is clicked

--- docs/index.js
+++ docs/index.js
@@ -14,3 +14,18 @@ document.querySelector('#mobile-close-icon').onclick = function() {
     document.querySelector('#body').style.overflow = 'auto'; 
     document.querySelector('#hamburger-and-logo').style.visibility = 'visible';
 }
+
+for(let i = 1; i <= 4; i++){
+    const currentArrow = document.getElementById(`arrow-${String(i)}`);
+    const currentArrowDown = document.getElementById(`arrow-down-${String(i)}`);
+
+    document.getElementById(`question-${String(i)}`).onclick = function(){
+        if(currentArrow.style.display !== 'none'){
+            currentArrow.style.display = 'none';
+            currentArrowDown.style.display = 'block';
+        }else{
+            currentArrow.style.display = 'block';
+            currentArrowDown.style.display = 'none';
+        }
+    }
+}
