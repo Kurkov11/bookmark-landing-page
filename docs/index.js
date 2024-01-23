d5d9722311b78bfcfa50c3678075aed80bc83b8d
Kurkov11 <kurkovsw@icloud.com>
2024-01-23 10:01:20 +0100

    Hide hamnurger and logo when mobile menu is shown to show only one copy of logo and menu icon at a time

--- docs/index.js
+++ docs/index.js
@@ -7,8 +7,10 @@ for(const faqOption of faqOptions){
 document.querySelector('#mobile-menu-icon').onclick = function() {
     document.querySelector('#mobile-menu').style.display = 'flex';
     document.querySelector('#body').style.overflow = 'hidden';
+    document.querySelector('#hamburger-and-logo').style.visibility = 'hidden';
 }
 document.querySelector('#mobile-close-icon').onclick = function() {
     document.querySelector('#mobile-menu').style.display = 'none';
     document.querySelector('#body').style.overflow = 'auto'; 
+    document.querySelector('#hamburger-and-logo').style.visibility = 'visible';
 }
